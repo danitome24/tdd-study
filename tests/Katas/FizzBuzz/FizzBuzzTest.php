c01f0121ec7a940fc28f3da77a2a0187e2fd7817
danieltomefernandez <danieltomefer@gmail.com>
2022-05-18 10:47:38 +0200

    Fizz Buzz test example

--- tests/Katas/FizzBuzz/FizzBuzzTest.php
+++ tests/Katas/FizzBuzz/FizzBuzzTest.php
@@ -2,8 +2,12 @@
 
 namespace Tests\App\Katas\FizzBuzz;
 
+use App\FizzBuzz\FizzBuzz;
 use PHPUnit\Framework\TestCase;
 
-class FizzBuzzTest extends TestCase
-{
+class FizzBuzzTest extends TestCase {
+    /** @test */
+    public function canInstantiateFizzBuzz() {
+        $this->assertTrue(class_exists(FizzBuzz::class), 'Class FizzBuzz does not exists yet.');
+    }
 }
