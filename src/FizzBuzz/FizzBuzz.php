c97671f26a3284f8d5d16c3f06d6096aa8d4376b
danieltomefernandez <danieltomefer@gmail.com>
2022-05-18 11:14:29 +0200

    Fizz Buzz test example

--- src/FizzBuzz/FizzBuzz.php
+++ src/FizzBuzz/FizzBuzz.php
@@ -6,6 +6,6 @@ class FizzBuzz {
     const NUMBER_OF_ELEMENTS = 100;
 
     public function generate() {
-        return array_fill(0, self::NUMBER_OF_ELEMENTS, 'fizzbuzz');
+        return array_fill(0, self::NUMBER_OF_ELEMENTS, '1');
     }
 }
--- tests/Katas/FizzBuzz/FizzBuzzTest.php
+++ tests/Katas/FizzBuzz/FizzBuzzTest.php
@@ -13,4 +13,16 @@ class FizzBuzzTest extends TestCase {
         $fizzBuzz = new FizzBuzz();
         $this->assertCount(self::NUMBER_OF_ELEMENTS, $fizzBuzz->generate());
     }
+
+    /** @test */
+    public function numberOneIsRepresentedAsItself() {
+        $number = 1;
+        $this->assertNumberIsRepresentedAsItself($number, '1');
+    }
+
+    private function assertNumberIsRepresentedAsItself(int $number, string $representation): void {
+        $fizzBuzz = new FizzBuzz();
+        $numberList = $fizzBuzz->generate();
+        $this->assertEquals($representation, $numberList[$number - 1]);
+    }
 }
